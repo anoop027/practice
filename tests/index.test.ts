065c10a93adcca2d8171ba5b0751574deb996ccf
anoop027 <74642411+anoop027@users.noreply.github.com>
2024-10-17 12:37:36 +0530

    Update index.test.ts

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -2,8 +2,20 @@ import request from 'supertest';
 import app from '../src/index';
 
 describe('GET /', () => {
+    let server: any;
+
+    beforeAll((done) => {
+        server = app.listen(4000, () => {
+            done();
+        });
+    });
+
+    afterAll((done) => {
+        server.close(done);
+    });
+
     it('should return Hello, World!', async () => {
-        const res = await request(app).get('/');
+        const res = await request(server).get('/');
         expect(res.statusCode).toEqual(200);
         expect(res.text).toBe('Hello, World!');
     });
