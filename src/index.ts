ab9ae7470733f4e97a21af5ddc755972ec94fa47
anoop027 <74642411+anoop027@users.noreply.github.com>
2024-10-17 13:10:12 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,4 @@ app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
-
 export default app;
