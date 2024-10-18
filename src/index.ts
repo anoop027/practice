4474e1b79bba86edbc3055abe6f00f414cb755d4
anoop027 <74642411+anoop027@users.noreply.github.com>
2024-10-18 15:27:21 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+const express = require('express');
 
 const app = express();
 
@@ -6,11 +6,11 @@ app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
-// Add this part to listen on the port specified by the environment variable
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
 
-export default app;
+module.exports = app;
+
 
