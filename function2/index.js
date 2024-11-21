b357317ac6482038bf53785557f6eaaceca3f7e7
anoop027 <74642411+anoop027@users.noreply.github.com>
2024-11-21 15:01:10 +0530

    Create index.js

---
+++ function2/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('test codecdcd!');
+});
+
+// const port = process.env.PORT || 8080;
+// app.listen(port, () => {
+//     console.log(`Server is running on port ${port}`);
+// });
+
+module.exports = { nodefunction2: app };  // Export the function as nodefunction
